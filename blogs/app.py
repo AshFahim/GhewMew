4f1f70e158476b2361198ebb9372bcfe4f0ec1a3
AshFahim <fahimashiqurrahman@gmail.com>
2024-03-01 23:05:34 +0600

    update(blog): created api for putting data in blogs database

--- blogs/app.py
+++ blogs/app.py
@@ -1,13 +1,43 @@
 import uvicorn
 from fastapi import FastAPI
 from lib.db import cursor 
+from fastapi import HTTPException
 
+class Blog:
+    title: str
+    content: str
+    author: str 
+    created_at: str
+    
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
     return {"message": "Auth server is running"}
+
+@app.put("/addblog")
+async def add_blog(blog:Blog):
+    # validate incoming data from Blog 
+    if not([blog.title, blog.content, blog.author, blog.created_at]):
+        raise HTTPException(status_code=400, detail="All fields are required")
+    
+    values = (blog.title, blog.content, blog.author, blog.created_at)
+    
+    query = """ 
+    INSERT INTO `blogs` (`title`, `content`, `author`, `created_at`)
+    VALUES (%s, %s, %s, %s)
+    """
+    try:
+        cursor.execute(query, values)
+        return {"message": "Blog added successfully"}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail="Internal server error")
+    
+    return ("message", "Blog added successfully")
+
+    
+    
 
 
 #python -m uvicorn app:app --reload --port 3700
