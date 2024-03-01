03df91fe2e8f6c6ceebb51a490aa8fd2f463253c
BansheeScreamss <mehnajmissal@gmail.com>
2024-03-01 22:24:54 +0600

    update(blogs):updated database for blogs

---
+++ blogs/lib/db.py
@@ -0,0 +1,40 @@
+from dotenv import load_dotenv, dotenv_values
+import MySQLdb
+import os
+
+config = dotenv_values(".venv")
+
+connection = MySQLdb.connect(
+    host = "localhost",
+    user = "root",
+    password = "",
+    database = "test",
+    autocommit = True 
+)
+
+cursor  = connection.cursor()
+
+if connection:
+    print("Connection is succesful")
+else:
+    print("no connection")
+    
+create_tabel_query = """
+    CREATE TABLE IF NOT EXISTS `blogs` (
+        `id` int(11) NOT NULL AUTO_INCREMENT,
+        `title` varchar(255) NOT NULL,
+        `content` varchar(2048) NOT NULL,
+        `author` varchar(32) NOT NULL,
+        `created_at` varchar(16) NOT NULL,
+        PRIMARY KEY (`id`)
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+"""
+
+cursor.execute(create_tabel_query)
+
+
+
+
+
+
+
