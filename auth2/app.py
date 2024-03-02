53a6f652c641f95619546c6af4362b32201179ef
AshFahim <fahimashiqurrahman@gmail.com>
2024-03-02 06:58:21 +0600

    update(auth2): fixed adduser

--- auth2/app.py
+++ auth2/app.py
@@ -5,16 +5,16 @@ from fastapi import  HTTPException
 from datatype.datatype import UserModel_Login, UserModel_Signup, UserModel_Remove
 app = FastAPI()
 
-usertype = ["admin", "user"]
 
 @app.get("/")
 async def root():
     return {"message": "Auth server is running"}
 
-@app.post("/adduser/{usertype}")
+@app.post("/adduser/")
 async def create_user(user: UserModel_Signup):
-    if usertype not in usertype:
-        raise HTTPException(status_code=400, detail="Invalid user type")
+    #if usertype not in ["user", "admin"]:
+        #raise HTTPException(status_code=400, detail="Invalid user type")
+        #usertype = "user"
     
     if not ([user.name, user.email, user.password]):
         raise HTTPException(status_code=400, detail="All fields are required")
@@ -23,7 +23,7 @@ async def create_user(user: UserModel_Signup):
             INSERT INTO users (name, email, password, user_type)
             VALUES (%s, %s, %s, %s)
         """
-    values = (user.name, user.email, user.password, usertype)
+    values = (user.name, user.email, user.password, "user")
 
     try:
         cursor.execute(query, values)
