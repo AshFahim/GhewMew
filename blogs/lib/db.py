c7bf73dd128f23374de77413c2f005a0feb0f75f
BansheeScreamss <mehnajmissal@gmail.com>
2024-04-06 01:24:44 +0600

    update(blogs): modified database

--- blogs/lib/db.py
+++ blogs/lib/db.py
@@ -21,13 +21,14 @@ else:
     
 create_tabel_query = """
     CREATE TABLE IF NOT EXISTS `blogs` (
-        `id` int(11) NOT NULL AUTO_INCREMENT,
-        `title` varchar(255) NOT NULL,
-        `content` varchar(2048) NOT NULL,
-        `author` varchar(32) NOT NULL,
-        `created_at` varchar(16) NOT NULL,
-        PRIMARY KEY (`id`)
-    ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `title` varchar(255) NOT NULL,
+    `content` varchar(2048) NOT NULL,
+    `author` varchar(32) NOT NULL,
+    `created_at` varchar(16) NOT NULL,
+    `image_path` varchar(255),
+    PRIMARY KEY (`id`)
+)  ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 """
 
 cursor.execute(create_tabel_query)
