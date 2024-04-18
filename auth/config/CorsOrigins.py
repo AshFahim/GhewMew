b4204a1fd74247ba14ef9c50e8744cb5376e7857
AshFahim <fahimashiqurrahman@gmail.com>
2024-04-18 18:42:24 +0600

    Removed auth

--- auth/config/CorsOrigins.py
+++
@@ -1,9 +0,0 @@
-origins = [
-    "*",
-    "http://localhost",
-    "http://localhost:8080",
-    "http://localhost:8000",
-    "http://localhost:3000",
-    "http://localhost:5000"
-    # add other origins here
-]
